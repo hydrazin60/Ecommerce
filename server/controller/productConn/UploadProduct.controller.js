47707de8a87b5c94533781d891657b076b20019d
hydrazin60 <sureshkhdk45@gmail.com>
2024-07-17 08:57:40 +0545

    UploadProduct frontend ui complide

---
+++ server/controller/productConn/UploadProduct.controller.js
@@ -0,0 +1,14 @@
+export const UploadProductController = async (req, res) => {
+  try {
+
+
+    
+
+  } catch (err) {
+    res.status(400).json({
+      message: err.message || err,
+      error: true,
+      success: false,
+    });
+  }
+};
